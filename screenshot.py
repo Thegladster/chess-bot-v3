025141d4a6084dc346a59845a25fc560ff2f22b5
Thegladster <64565266+Thegladster@users.noreply.github.com>
2024-05-14 18:12:56 -0700

    Add files via upload

---
+++ screenshot.py
@@ -0,0 +1,34 @@
+import pyautogui
+import torch
+from PIL import ImageGrab
+import pathlib
+
+# CHANGE LEFT_OFFSET, TOP_OFFSET, AND SQUARE_SIDE
+square_side = 215
+left_offset = 564
+top_offset = 255
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+monwidth = pyautogui.size()[0]
+monheight = pyautogui.size()[1]
+right_offset = monwidth - (left_offset + square_side * 8)
+bottom_offset = (monheight - top_offset - square_side * 8)
+
+print("Loading model...")
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/LY GAMING PC/yolov5/best.pt')
+model.conf = 0.8
+
+def screenshot():
+    global df
+
+    # Take a screenshot of window
+    chessboard = (left_offset, top_offset, monwidth - right_offset, monheight - bottom_offset)
+    im = ImageGrab.grab(chessboard)
+
+    # Inference
+    results = model(im)
+    results.show()
+
+screenshot()
